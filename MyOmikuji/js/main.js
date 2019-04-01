7b032f58e67c0c3ba3764e943416314f3ee05921
English <tailangs97@gmail.com>
2019-04-01 14:23:03 +0900

    配列を用いたおみくじ機能の作成

--- MyOmikuji/js/main.js
+++ MyOmikuji/js/main.js
@@ -4,6 +4,16 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
-    btn.textContent = 'hit!';
+    const results = ['大吉', '中吉', '凶', '末吉'];
+    const n = Math.floor(Math.random() * results.length);
+    btn.textContent = results[n];
+  });
+
+  btn.addEventListener('mousedown', () => {
+    btn.classList.add('pressed');
+  });
+
+  btn.addEventListener('mouseup', () => {
+    btn.classList.remove('pressed');
   });
 }
